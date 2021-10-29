c8c20c17e23e833ddc4eadbc54e940cc798c45e2
Ignacio Heredia <iheredia@ifca.unican.es>
2021-10-29 15:43:10 +0200

    add more data types

--- README.md
+++ README.md
@@ -3,9 +3,7 @@ Demo App
 
 [![Build Status](https://jenkins.indigo-datacloud.eu/buildStatus/icon?job=Pipeline-as-code/DEEP-OC-org/demo_app/master)](https://jenkins.indigo-datacloud.eu/job/Pipeline-as-code/job/DEEP-OC-org/job/demo_app/job/master)
 
-> :warning: This is **work-in-progress**.
-
-A _minimal_ toy application for demo and testing purposes. We just implemented dummy inference, ie. we return the same inputs we are feed.
+A _minimal_ toy application for demo and testing purposes. We just implemented dummy inference, ie. we return the same inputs we are feed. If some input is not feed we generate a default one.
 
 To launch it, first install the package then run deepaas:
 ```bash
@@ -14,7 +12,10 @@ pip install -e .
 deepaas-run --listen-ip 0.0.0.0
 ```
 
-> **TODOs**
-> * implement additional data types
-> * add dummy model metadata
-> * create DEEP-OC repo to launch with Docker
+Samples for media files are provided in `./data`.
+
+> :warning: This is **work-in-progress**.
+>
+> **TODOs**:
+> * implement additional data types (if needed).
+> * create DEEP-OC repo to launch with Docker.
--- demo_app/api.py
+++ demo_app/api.py
@@ -1,18 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 Spanish National Research Council (CSIC)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 """
 Integrate a model with the DEEP API
 """
 
-import argparse
+import base64
 from functools import wraps
 import json
 import pkg_resources
+from random import random
 
 from aiohttp.web import HTTPBadRequest
-from webargs import fields
-
-# import demo_app.config as cfg
-
-print('#' * 100)
+from webargs import fields, validate
 
 
 def _catch_error(f):
@@ -27,103 +40,94 @@ def _catch_error(f):
     return wrap
 
 
-# def _fields_to_dict(fields_in):
-#     """
-#     Example function to convert mashmallow fields to dict()
-#     """
-#     dict_out = {}
-    
-#     for key, val in fields_in.items():
-#         param = {}
-#         param['default'] = val.missing
-#         param['type'] = type(val.missing)
-#         if key == 'files' or key == 'urls':
-#             param['type'] = str
-
-#         val_help = val.metadata['description']
-#         if 'enum' in val.metadata.keys():
-#             val_help = "{}. Choices: {}".format(val_help, 
-#                                                 val.metadata['enum'])
-#         param['help'] = val_help
-
-#         try:
-#             val_req = val.required
-#         except:
-#             val_req = False
-#         param['required'] = val_req
-
-#         dict_out[key] = param
-#     return dict_out
-
-
-# def get_metadata():
-#     """
-#     Function to read metadata
-#     https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/latest/user/v2-api.html#deepaas.model.v2.base.BaseModel.get_metadata
-#     :return:
-#     """
-
-#     module = __name__.split('.', 1)
-
-#     try:
-#         pkg = pkg_resources.get_distribution(module[0])
-#     except pkg_resources.RequirementParseError:
-#         # if called from CLI, try to get pkg from the path
-#         distros = list(pkg_resources.find_distributions(cfg.BASE_DIR, 
-#                                                         only=True))
-#         if len(distros) == 1:
-#             pkg = distros[0]
-#     except Exception as e:
-#         raise HTTPBadRequest(reason=e)
-
-#     predict_args = get_predict_args()
-    
-#     for key, val in predict_args.items():
-#         predict_args[key]['type'] = str(val['type'])  # make 'type' JSON serializable
-
-#     meta = {
-#         'name': None,
-#         'version': None,
-#         'summary': None,
-#         'home-page': None,
-#         'author': None,
-#         'author-email': None,
-#         'license': None,
-#         'help-train' : train_args,
-#         'help-predict' : predict_args
-#     }
-
-#     for line in pkg.get_metadata_lines("PKG-INFO"):
-#         line_low = line.lower() # to avoid inconsistency due to letter cases
-#         for par in meta:
-#             if line_low.startswith(par.lower() + ":"):
-#                 _, value = line.split(": ", 1)
-#                 meta[par] = value
-
-#     return meta
-
-
 def get_metadata():
-    print('+' * 100)
     metadata = {
-        "author": "Author name",
-        "description": "Model description",
-        "license": "Model's license",
-        "url": "URL for the model (e.g. GitHub repository)",
-        "version": "Model version",
+        "author": "Ignacio Heredia",
+        "description": 
+            """
+            A minimal toy application for demo and testing purposes.
+            We just implemented dummy inference, ie. we return the same inputs we are feed.
+            """,
+        "license": "MIT",
+        "url": "https://github.com/deephdc/demo_app",
+        "version": "0.1",
+        "summary": 
+            """
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised
+            in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
+            """,
     }
     return metadata
 
 
-
 def get_predict_args():
-    
+    """
+    TODO: add more dtypes
+    * image, audio, video
+    * int with choices, int with range
+    * composed: list of strs, list of int
+    """
+    # WARNING: missing!=None has to go with required=False
     arg_dict = {
-        "arg1": fields.Str(
+        "demo-str": fields.Str(
             required=False,
-            missing=None,
-            enum=["choice1", "choice2"],  # list of choices
+            missing='some-string',
         ),
+        "demo-str-choice": fields.Str(
+            required=False,
+            missing='choice2',
+            enum=["choice1", "choice2"],
+        ),
+        "demo-int": fields.Int(
+            required=False,
+            missing=1,
+        ),
+        "demo-int-range": fields.Int(
+            required=False,
+            missing=50,
+            validate=[validate.Range(min=1, max=100)]
+        ),
+        "demo-float": fields.Float(
+            required=False,
+            missing=0.1,
+        ),
+        "demo-bool": fields.Bool(
+            required=False,
+            missing=True,
+        ),
+        "demo-dict": fields.Str(  # dicts have to be processed as strings 
+            required=False,
+            missing='{"a": 0, "b": 1}',  # use double quotes inside dict
+        ),
+        "demo-list-of-floats": fields.List(
+            fields.Float(),
+            required=False,
+            missing=[0.1, 0.2, 0.3],
+        ),
+        "demo-image": fields.Field(
+            required=False,
+            type="file",
+            location="form",
+            description="image",  # needed to be parsed by UI
+        ),
+        "demo-audio": fields.Field(
+            required=False,
+            type="file",
+            location="form",
+            description="audio",  # needed to be parsed by UI
+        ),
+#         "demo-video": fields.Field(
+#             required=False,
+#             type="file",
+#             location="form",
+#             description="video",  # needed to be parsed by UI
+#         ),    
     }
     
     return arg_dict
@@ -132,76 +136,45 @@ def get_predict_args():
 @_catch_error
 def predict(**kwargs):
     """
-    Function to execute prediction
-    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/latest/user/v2-api.html#deepaas.model.v2.base.BaseModel.predict
-    :param kwargs:
-    :return:
+    Return same inputs as provided. We also add additional fields
+    to test the functionality of the Gradio-based UI [1].
+       [1]: https://github.com/deephdc/deepaas_ui
     """
+    # Dict are fed as str so have to be converted back
+    kwargs['demo-dict'] = json.loads(kwargs['demo-dict'])
 
-    return kwargs['arg1']
-
-
-
-# def main():
-#     """
-#     Runs above-described methods from CLI
-#     (see below an example)
-#     """
-
-#     if args.method == 'get_metadata':
-#         meta = get_metadata()
-#         print(json.dumps(meta))
-#         return meta
-#     elif args.method == 'predict':
-#         # [!] you may need to take special care in the case of args.files [!]
-#         results = predict(**vars(args))
-#         print(json.dumps(results))
-#         return results
-
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='Model parameters', 
-#                                      add_help=False)
-
-#     cmd_parser = argparse.ArgumentParser()
-#     subparsers = cmd_parser.add_subparsers(
-#                             help='methods. Use \"deep_api.py method --help\" to get more info', 
-#                             dest='method')
-
-#     ## configure parser to call get_metadata()
-#     get_metadata_parser = subparsers.add_parser('get_metadata', 
-#                                          help='get_metadata method',
-#                                          parents=[parser])
-#     # normally there are no arguments to configure for get_metadata()
-
-#     ## configure arguments for predict()
-#     predict_parser = subparsers.add_parser('predict', 
-#                                            help='commands for prediction',
-#                                            parents=[parser]) 
-#     # one should convert get_predict_args() to add them in predict_parser
-#     # For example:
-#     predict_args = _fields_to_dict(get_predict_args())
-#     for key, val in predict_args.items():
-#         predict_parser.add_argument('--%s' % key,
-#                                default=val['default'],
-#                                type=val['type'],
-#                                help=val['help'],
-#                                required=val['required'])
-
-#     ## configure arguments for train()
-#     train_parser = subparsers.add_parser('train', 
-#                                          help='commands for training',
-#                                          parents=[parser]) 
-#     # one should convert get_train_args() to add them in train_parser
-#     # For example:
-#     train_args = _fields_to_dict(get_train_args())
-#     for key, val in train_args.items():
-#         train_parser.add_argument('--%s' % key,
-#                                default=val['default'],
-#                                type=val['type'],
-#                                help=val['help'],
-#                                required=val['required'])
-
-#     args = cmd_parser.parse_args()
+    # Add labels and random probalities to output as mock
+    prob = [random() for _ in range(5)]
+    kwargs['probabilities'] = [i / sum(prob) for i in prob]
+    kwargs['labels'] = ['class2', 'class3', 'class0', 'class1', 'class4']
     
-#     main()
+    # Read media files and return them back in base64
+    for k in ['demo-image', 'demo-audio']:
+#     for k in ['demo-video']:
+        with open(kwargs[k].filename, 'rb') as f:
+            media = f.read()
+        media = base64.b64encode(media)  # bytes
+        kwargs[k] = media.decode('utf-8')  # string (in utf-8)
+    
+    return kwargs
+
+
+# Schema to validate the `predict()` output 
+schema = {
+    "demo-str": fields.Str(),
+    "demo-str-choice": fields.Str(),
+    "demo-int": fields.Int(),
+    "demo-int-range": fields.Int(),
+    "demo-float": fields.Float(),
+    "demo-bool": fields.Bool(),
+    "demo-dict": fields.Dict(),
+    "demo-list-of-floats": fields.List(fields.Float()),
+    "demo-image": fields.Str(
+        description='image'),  # needed to be parsed by UI
+    "demo-audio": fields.Str(
+        description='audio'),  # needed to be parsed by UI
+#     "demo-video": fields.Str(
+#         description='video'),  # needed to be parsed by UI
+    "labels": fields.List(fields.Str()),
+    "probabilities": fields.List(fields.Float()),
+}
